9e19b506edc2ffa656680883dee9a8c262fb6897
daniherr <daniherr@bancolombia.com.co>
2023-04-05 09:34:20 -0500

    feat 🆕: Se agrega el esquema de colores y el tema de los textos

--- weinflu_app/lib/main.dart
+++ weinflu_app/lib/main.dart
@@ -1,22 +1,33 @@
 // Importamos el sistema de desiño de Google y todos
 // sus componentes reutilizables
 import 'package:flutter/material.dart';
+import 'package:weinflu_app/design/colors.dart';
 
 //Punto de entrada de la aplicación aqui inicia toda la magia
 void main() {
   runApp(MaterialApp(
+    theme: ThemeData(
+      fontFamily: 'RobotoMono',
+      primaryColor: WeinFluColors.brandPrimaryColor,
+      textTheme: const TextTheme(
+        displayLarge: TextStyle(fontSize: 48.0, fontWeight: FontWeight.bold),
+        displayMedium: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
+        displaySmall: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold),
+        headlineLarge: TextStyle(fontSize: 21.0, color: WeinFluColors.brandPrimaryColor),
+        bodyLarge: TextStyle(fontSize: 14.0),
+        bodyMedium: TextStyle(fontSize: 13.0),
+        bodySmall: TextStyle(fontSize: 10.0),
+      ),
+    ),
     home: Scaffold(
-      backgroundColor: Colors.grey[200],
       appBar: AppBar(
         title: const Text(
           'Weinflu App 💙',
-          style: TextStyle(fontSize: 34, color: Colors.white),
         ),
       ),
       body: const Center(
           child: Text(
-        'hola Weincoders 🚀!!!',
-        style: TextStyle(fontSize: 34),
+        'Hola Weincoders 🚀!!!',
       )),
     ),
   ));
