8ccf2815b912d2cf59f1563d930c7cc75f078b50
unknown <sebastianewilches@hotmail.com>
2023-08-10 16:49:23 -0500

    First push

---
+++ basicflutter-app/android/app/src/main/kotlin/com/example/sewil_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.sewil_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- weinflu_app/lib/main.dart
+++
@@ -1,9 +0,0 @@
-// Importamos el sistema de desiño de Google y todos
-// sus componentes reutilizables
-import 'package:flutter/material.dart';
-import 'package:weinflu_app/app.dart';
-
-//Punto de entrada de la aplicación aqui inicia toda la magia
-void main() {
-  runApp(const WeinFluApp());
-}
